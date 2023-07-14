13137743da97236b83b86e4461809bd9410cf14b
Mohit Mehra <98013205+M0hitMehra@users.noreply.github.com>
2023-07-14 21:59:41 +0530

    routes corrected

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,14 +1,14 @@
 import express from "express";
 import {isAuthenticated}  from "../middlewares/auth.js"
-import { createBlog, deleteBlog, getAllBlogs, updateBlog } from "../controllers/blogController.js";
+import { createBlog, deleteBlog, getAllBlogs, getSpecificBlogs, updateBlog } from "../controllers/blogController.js";
 
 const router =  express.Router();
 
 router.route("/create-blog").post(isAuthenticated,createBlog)
 router.route("/update-blog/:_id").put(isAuthenticated,updateBlog)
 router.route("/delete-blog/:_id").delete(isAuthenticated,deleteBlog)
-router.route("/get-blogs").put(getAllBlogs)
-router.route("/get-blog/:_id").put(getAllBlogs)
+router.route("/get-blogs").get(getAllBlogs)
+router.route("/get-blog/:_id").get(getSpecificBlogs)
 
 
 export default router
